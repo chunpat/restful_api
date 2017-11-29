4c9fd4c66ef06c69b442310c544bb91888c011bd
Your Name <398949389@qq.com>
2017-11-29 23:12:33 +0800

    it is time to update my github,how long time not to update,haha

---
+++ app/api/service/WxNotify.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ZZHPENG
+ * Date: 2017/11/24
+ * Time: 23:27
+ */
+
+namespace app\api\service;
+
+
+class WxNotify
+{
+
+}
---
+++ app/api/validate/PagingParameter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ZZHPENG
+ * Date: 2017/11/29
+ * Time: 16:28
+ */
+
+namespace app\api\validate;
+
+
+class PagingParameter
+{
+
+}
